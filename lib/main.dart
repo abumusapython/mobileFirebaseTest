c910c9f761582da29055758697e84692fba5fa15
MD. ABU MUSA <this.abumusa@gmail.com>
2025-09-27 11:40:57 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_test/firebase_options.dart';
+import 'package:mobile_test/practice%20firebas/add_acreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+
+      home: Builder(
+        builder: (context) {
+          return const AddAcreen();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/practice firebas/add_acreen.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddAcreen extends StatefulWidget {
+  const AddAcreen({super.key});
+
+  @override
+  State<AddAcreen> createState() => _AddAcreenState();
+}
+
+class _AddAcreenState extends State<AddAcreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Login", // responsive text
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 50),
+
+            /// Email
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 20),
+
+            /// Password
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            /// Login Button
+            ElevatedButton(
+              onPressed: () {
+                addUserData(emailController.text, passwordController.text);
+              },
+              child: Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  addUserData(String email, String password) async {
+    await FirebaseFirestore.instance.collection("userLoginData").add({
+      'email': email,
+      'password': password,
+    });
+
+    emailController.clear();
+    passwordController.clear();
+  }
+}
