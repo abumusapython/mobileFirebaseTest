718c82893a7405b3bbd244cf2f97dd7d75969d5c
MD. ABU MUSA <this.abumusa@gmail.com>
2025-09-27 11:42:03 +0600

    'mt'

--- lib/practice firebas/add_acreen.dart
+++ lib/practice firebas/add_acreen.dart
@@ -58,7 +58,7 @@ class _AddAcreenState extends State<AddAcreen> {
               onPressed: () {
                 addUserData(emailController.text, passwordController.text);
               },
-              child: Text("Login"),
+              child: Text("LoginTest"),
             ),
           ],
         ),
